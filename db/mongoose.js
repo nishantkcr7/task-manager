d977f26713cff79a66a909ff0b6eeab99779c288
Nishantkcr7 <nishantkcr7@gmail.com>
2022-06-27 16:19:59 +0530

    Uninstalled mongoose

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,27 +1,28 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",
-{
-    useNewUrlParser:true,
-    useCreateIndex:true
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
 });
 
-const Users = mongoose.model('Users',{
-    name: {
-        type:String
-    },
-    age: {
-        type: Number
-    }
-})
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
 
-const me = new Users({
-    name: 'Nishant',
-    age: 22
-})
+const me = new User({
+  name: "Nishant",
+  age: 22,
+});
 
-me.save().then(()=>{
+me.save()
+  .then(() => {
     console.log(me);
-}).catch((error)=>{
+  })
+  .catch((error) => {
     console.log(e);
-})
+  });
